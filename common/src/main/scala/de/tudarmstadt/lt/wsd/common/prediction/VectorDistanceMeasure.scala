0477dce08170ae5ab7106ac82471889921f94638
Fide Marten <fide.marten@stud.tu-darmstadt.de>
2017-06-20 09:59:12 +0200

    Fix NaiveBayes measure for empty context

--- common/src/main/scala/de/tudarmstadt/lt/wsd/common/prediction/VectorDistanceMeasure.scala
+++ common/src/main/scala/de/tudarmstadt/lt/wsd/common/prediction/VectorDistanceMeasure.scala
@@ -1,6 +1,6 @@
 package de.tudarmstadt.lt.wsd.common.prediction
 import breeze.linalg.{Vector => BV}
-import Math.log
+import Math.{log, exp}
 
 import com.typesafe.scalalogging.LazyLogging
 /**
@@ -13,12 +13,16 @@ object VectorDistanceMeasure extends LazyLogging {
   val random: VectorDistanceMeasure = (v1: BV[Double], v2: BV[Double]) => scala.util.Random.nextDouble()
 
   val naivebayes: (Double) => VectorDistanceMeasure = (priorV1: Double) => (v1: BV[Double], v2: BV[Double]) => {
-    val pairwise = hadamardProduct(v1, v2)
-    val numMissing = v1.activeSize - pairwise.activeSize
+    if (v1.activeSize == 0)
+      0 // If v1 is empty (i.e. context was empty or all out-off-voc-words), the result would be exp(0) = 1, better to return 0 then
+    else {
+      val pairwise = hadamardProduct(v1, v2)
+      val numMissing = v1.activeSize - pairwise.activeSize
 
-    val existingLogs = pairwise.activeValuesIterator.toList.map(log)
+      val existingLogs = pairwise.activeValuesIterator.toList.map(log)
 
-    existingLogs.fold(0.0)(_ + _) + (log(priorV1) * numMissing)
+      exp(existingLogs.fold(0.0)(_ + _) + (log(priorV1) * numMissing))
+    }
   }
 
   def hadamardProduct: (BV[Double], BV[Double]) => BV[Double] = (v1: BV[Double], v2: BV[Double]) => v1 :* v2
