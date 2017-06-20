622a967420ccf6b341c9c285654b076711c38c63
Fide Marten <fide.marten@stud.tu-darmstadt.de>
2017-06-20 09:51:34 +0200

    Log FeatureVectorizer creation

--- common/src/main/scala/de/tudarmstadt/lt/wsd/common/FeatureVectorizer.scala
+++ common/src/main/scala/de/tudarmstadt/lt/wsd/common/FeatureVectorizer.scala
@@ -4,18 +4,22 @@ import breeze.linalg.{SparseVector, Vector => BV}
 import com.typesafe.scalalogging.LazyLogging
 import de.tudarmstadt.lt.wsd.common.model.{IndexedFeature, WordVector}
 
-object FeatureVectorizer {
-  var vectorizerMap: Map[WordVector.ModelName, FeatureVectorizer] =
-    Map.empty[WordVector.ModelName, FeatureVectorizer]
+object FeatureVectorizer extends LazyLogging {
+  var instances: Map[WordVector.ModelName, FeatureVectorizer] = Map()
 
-  def getVectorizer(wordVectorModel: WordVector.ModelName): FeatureVectorizer = {
-    vectorizerMap.get(wordVectorModel) match {
-      case Some(vectorizer) => vectorizer
+  def getVectorizer(model: WordVector.ModelName): FeatureVectorizer = {
+    instances get model match {
+
+      case Some(vectorizer) =>
+        logger.info(s"Using cached FeatureVectorizer for model '$model'")
+        vectorizer
+
       case None =>
-          val labels = IndexedFeature.findAllByModelSorted(wordVectorModel).map(_.label).toArray
-          val vectorizer = new FeatureVectorizer(labels)
-          vectorizerMap += (wordVectorModel -> vectorizer)
-          vectorizer
+        val labels = IndexedFeature.findAllByModelSorted(model).map(_.label).toArray
+        val vectorizer = new FeatureVectorizer(labels)
+        instances += (model -> vectorizer)
+        logger.info(s"No FeatureVectorizer existed for model '$model', had to create it!")
+        vectorizer
     }
   }
 }
