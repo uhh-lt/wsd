febc9926a4d5dc62fc06bc280531d26af5d7c7f6
Fide Marten <fide.marten@stud.tu-darmstadt.de>
2017-07-26 15:59:02 +0200

    Fix typos in web frontend

--- web/src/model-descriptions.js
+++ web/src/model-descriptions.js
@@ -13,11 +13,11 @@ const modelDescriptions = {
         text: <span>This model uses the <b>cluster words</b> from the induced <b>word sense inventory</b> as sparse features that represent the sense. This model was induced from a text corpus which is a combination of Wikipedia, ukWaC, LCC News corpus, and Gigaword.</span>,
     },
     'cos_traditional_self': {
-        title: "Word Senses based on Cluster Word Features",
+        title: "Word Senses based on Context Word Features",
         text: <span>This representation is based on a sum of word vectors of all cluster words in the induced <b>word sense inventory</b> weighted by distributional similarity scores. Word vectors use <b>context words</b> (words that often co-occur in the text corpus with the word) as sparse features. This model was induced from a text corpus which is a combination of Wikipedia, ukWaC, LCC News corpus, and Gigaword.</span>,
     },
     'naivebayes_cosets1k_self': {
-        title: "Super Senses based on Context Word Features",
+        title: "Super Senses based on Cluster Word Features",
         text: <span>To build this model, induced word senses are first globally clustered using the Chinese Whispers graph clustering algorithm. The edges in this sense graph are established by disambiguation of the cluster words. The resulting clusters represent semantic classes grouping words sharing a common hypernym, e.g. all programming languages. This set of semantic classes is used as an automatically learned <b>inventory of super senses</b>: There is only one global sense inventory shared among all words in contrast to the two previous 'traditional' models. Each semantic class is labeled with hypernyms. This model uses <b>cluster words</b> belonging to the semantic class as features. This model was induced from a text corpus which is a combination of Wikipedia, ukWaC, LCC News corpus, and Gigaword.</span>,
     },
     'naivebayes_cosets1k_coocwords': {
--- web/src/samples.js
+++ web/src/samples.js
@@ -47,7 +47,40 @@ const samples = [
     {
         "context": "In geography, the word bank generally refers to the land alongside a body of water.",
         "word": "bank"
-    }
+    },
+    {
+        "context": "The Sun is the star at the center of the Solar System.",
+        "word": "sun"
+    },
+    {
+        "context": "Sun was an American company that sold computers, computer components, software, and information technology services, and that created the Java programming language, the Solaris operating system, ZFS, the Network File System (NFS) and SPARC.",
+        "word": "sun"
+    },
+    {
+        "context": "Apollo is one of the most important and complex of the Olympian deities in classical Greek and Roman religion and Greek and Roman mythology.",
+        "word": "apollo"
+    },
+    {
+        "context": "A bar is a retail business establishment that serves alcoholic beverages, such as beer, wine, liquor, cocktails, and other beverages such as mineral water and soft drinks and often sell snack foods such as crisps.",
+        "word": "bar"
+    },
+    {
+        "context": "Canon is a Japanese multinational corporation specialized in the manufacture of imaging and optical products, including cameras, camcorders, photocopiers, etc.",
+        "word": "canon"
+    },
+    {
+        "context": "A canon is a member of certain bodies subject to an ecclesiastical rule, a cleric living with others close to a cathedral and conducting his life according to the orders or rules of the church.",
+        "word": "canon"
+    },
+    {
+        "context": "Delphi is an integrated development environment (IDE) for desktop, mobile, web, and console applications.",
+        "word": "delphi"
+    },
+    {
+        "context": "Delphi is famous as the ancient sanctuary that grew rich as the seat of Pythia, the oracle consulted about important decisions throughout the ancient classical world.",
+        "word": "delphi"
+    },
+
 ];
 
 function randomElement(elements) {
