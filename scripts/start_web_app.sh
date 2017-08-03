b5742489c8bb5cf80d66fd7267a515e0c23e7ec6
Fide Marten <fide.marten@stud.tu-darmstadt.de>
2017-08-03 11:11:42 +0200

    Fix update mechanism in web app start script

--- scripts/start_web_app.sh
+++ scripts/start_web_app.sh
@@ -4,6 +4,6 @@
 # This will compile the SBT source code and generate a `Dockerfile` to `api/target/docker/stage`, which is used in the docker-compose.yml.
 docker run -it --rm -v $(pwd):/root hseeberger/scala-sbt sbt api/docker:stage -ivy .ivy2
 
-docker-compose build
-docker-compose up -d
-echo Web application started successfully.
+docker-compose up -d --build
+echo
+echo Web application successfully updated and started.
