157dc854d8856cb217aedd5e6d374ab4f3ef6d97
Fide Marten <fide.marten@stud.tu-darmstadt.de>
2017-09-04 18:44:25 +0200

    Check spark-sbumit on path or SPARK_HOME set

--- scripts/spark_submit_jar.sh
+++ scripts/spark_submit_jar.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 mkdir -p /tmp/spark-events
-spark_home=${SPARK_HOME:-/opt/spark-2.1.0-bin-hadoop2.7}
 
-# Note: Please include the following paramters if you have copied sample-app.conf to app.conf and want to use it!
-# --files app.conf --conf spark.executor.extraJavaOptions=-Dconfig.file=app.conf --conf spark.driver.extraJavaOptions=-Dconfig.file=app.conf
 
-$spark_home/bin/spark-submit \
+
+if which spark-submit > /dev/null; then
+  cmd=spark-submit
+else
+  cmd=${SPARK_HOME:-/opt/spark-2.1.0-bin-hadoop2.7}/bin/spark-submit
+
+  # Test command exists
+  which ${cmd} > /dev/null
+  if test $? -ne 0; then
+    echo
+    echo "Error: '\$SPARK_HOME' must be set to a valid spark installation, " \
+         "or 'spark-submit' be on your path."
+    exit 1
+  fi
+fi
+
+# Note: If you want to use nice-app.conf, please include the following parameters.
+# --files nice-app.conf
+# --conf spark.executor.extraJavaOptions=-Dconfig.file=nice-app.conf
+# --conf spark.driver.extraJavaOptions=-Dconfig.file=nice-app.conf
+
+${cmd} \
  --master "local[*]" \
  --conf 'spark.executor.extraJavaOptions=-Dlog4j.debug=true' \
  --conf 'spark.driver.extraJavaOptions=-Dlog4j.debug=true' \
