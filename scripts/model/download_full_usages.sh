c0b7de3de54cc92ec2ca81fca5ecb5bdeaa87e7e
Fide Marten <fide.marten@stud.tu-darmstadt.de>
2017-09-05 17:54:14 +0200

    Add check to avoid overriding already downloaded files

--- scripts/model/download_full_usages.sh
+++ scripts/model/download_full_usages.sh
@@ -3,5 +3,5 @@
 set -o nounset # Error on referencing undefined variables, shorthand: set -n
 set -o errexit # Abort on error, shorthand: set -e
 
-wget http://panchenko.me/data/joint/corpora/usages-wiki-ddt-mwe-313k.csv.gz -P data
+wget -c http://panchenko.me/data/joint/corpora/usages-wiki-ddt-mwe-313k.csv.gz -P data
 gunzip -c data/usages-wiki-ddt-mwe-313k.csv.gz > data/usages-wiki-ddt-mwe-313k.csv
